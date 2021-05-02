61de560e6a434ddf815c0082d68a915de0920344
Kirti Parihar <pariharkirti24@gmail.com>
2021-05-02 13:22:59 +0530

    Create admin in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+  name: 'Admin',
+  email: 'admin@example.com',
+  password: 'Test1234',
+  role: :admin
+)
